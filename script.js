25fadc4095489f49d8e5e3ee5fbb8493c7fb2706
nelizzy <nelizzy@hotmail.com>
2020-02-06 22:42:51 +0100

    Animate gif based on container scroll progress

--- script.js
+++ script.js
@@ -7,23 +7,38 @@ http://scrollmagic.io/docs/
 /* Get all Lottie animations on page */
 const animations = document.querySelectorAll('.lottie-animation');
 
-let animationsObject = {};
+/* An array that will hold the information of each animation */
+let animationsArray = [];
 
 /* Initialize each lottie animation */
 for (const animation of animations) {
   const lottieInstance = lottie.loadAnimation({
     container: animation,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
+    // renderer: 'svg',
+    loop: false,
+    autoplay: false,
     path: animation.dataset.animPath,
     name: animation.dataset.name
   })
 
-  const container = animation.parentElement;
-
-  animationsObject[animation.dataset.name] = {instance: lottieInstance, container: container};
+  animationsArray.push(lottieInstance);
 }
 
 /* Initialize ScrollMagic */
 const controller = new ScrollMagic.Controller();
+
+/* Build ScrollMagic Scene */
+animationsArray.forEach(anim => {
+  new ScrollMagic.Scene({
+      /* Animation will be linked to the scroll progress of its container, starting when the top of container is halfway through viewport and ending when bottom of container is halfway through viewport */
+      triggerElement: anim.wrapper.parentElement,
+      triggerHook: 0.5,
+      duration: anim.wrapper.parentElement.offsetHeight
+    })
+    .addTo(controller)
+    // .addIndicators() /* Debugging tool to see where and when animations occur */
+    .on("progress", function (e) {
+      const scrollFrame = e.progress * anim.totalFrames;
+      anim.goToAndStop(scrollFrame, true);
+    });
+})
