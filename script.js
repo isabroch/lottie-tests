0be6c80a3484ef8595680c9c6ab529566e60e15a
Isabella Broch <5460278@RTS-365.dk>
2020-02-07 14:53:15 +0100

    Finalize 'documentation'

--- script.js
+++ script.js
@@ -1,44 +1,85 @@
 /*
-http://airbnb.io/lottie/#/web
-http://scrollmagic.io/docs/
+ * Libraries used:
+ * http://airbnb.io/lottie/#/web
+ * http://scrollmagic.io/docs/
 */
-
 
 /* Get all Lottie animations on page */
 const animations = document.querySelectorAll('.lottie-animation');
 
-/* An array that will hold the information of each animation */
-let animationsArray = [];
-
-/* Initialize each lottie animation */
-for (const animation of animations) {
-  const lottieInstance = lottie.loadAnimation({
-    container: animation,
-    // renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: animation.dataset.animPath,
-    name: animation.dataset.name
-  })
-
-  animationsArray.push(lottieInstance);
-}
-
 /* Initialize ScrollMagic */
 const controller = new ScrollMagic.Controller();
 
-/* Build ScrollMagic Scene */
-animationsArray.forEach(anim => {
-  new ScrollMagic.Scene({
-      /* Animation will be linked to the scroll progress of its container, starting when the top of container is halfway through viewport and ending when bottom of container is halfway through viewport */
-      triggerElement: anim.wrapper.parentElement,
-      triggerHook: 0.5,
-      duration: anim.wrapper.parentElement.offsetHeight
-    })
-    .addTo(controller)
-    .addIndicators() /* Debugging tool to see where and when animations occur */
-    .on("progress", function (e) {
-      const scrollFrame = e.progress * (anim.totalFrames - 1);
-      anim.goToAndStop(scrollFrame, true);
-    });
+/* Initialize each lottie animation */
+animations.forEach(async (animation) => {
+  const lottieInstance = lottie.loadAnimation({
+    container: animation,
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: animation.dataset.animPath
+  })
+
+
+  const animDuration = (() => {
+    let duration;
+
+    if (animation.dataset.animDuration) {
+      duration = animation.dataset.animDuration;
+      const durationIsNumString = /^\d+$/.test(duration);
+      if ( durationIsNumString ) {
+        duration = parseInt(duration)
+      }
+    } else {
+      duration = "parentHeight"
+    }
+    return duration
+  })();
+
+  const animTriggerHook = (() => {
+    let triggerHook;
+
+    if (animation.dataset.animTriggerHook) {
+      triggerHook = animation.dataset.animTriggerHook;
+      triggerHook = parseFloat(triggerHook)
+    } else {
+      triggerHook = 0.5
+    }
+
+    return triggerHook
+  })();
+
+  const animOffset = (() => {
+    let offset;
+
+    if (animation.dataset.animOffset) {
+      offset = animation.dataset.animOffset;
+      offset = parseInt(offset)
+    } else {
+      offset = 0
+    }
+
+    return offset
+  })();
+
+
+  lottieInstance.addEventListener('DOMLoaded', async (e) => {
+    /* Waits until the SVGs have loaded to get height of container - prevents breaking of non-fixed height containers. */
+    let parent = animation.parentElement;
+
+    /* Build ScrollMagic scene once all SVGs are loaded*/
+    new ScrollMagic.Scene({
+        /* Animation will be linked to the scroll progress of its container, starting when the top of container is halfway through viewport and ending when bottom of container is halfway through viewport */
+        triggerElement: parent,
+        triggerHook: animTriggerHook,
+        offset: animOffset,
+        duration: animDuration === "parentHeight" ? parent.offsetHeight : animDuration === "elemHeight" ? animation.offsetHeight : animDuration
+      })
+      .addTo(controller)
+      .addIndicators() /* Debugging tool to see where an  d when animations occur */
+      .on("progress", function (e) {
+        const scrollFrame = e.progress * (lottieInstance.totalFrames - 1);
+        lottieInstance.goToAndStop(scrollFrame, true);
+      });
+  })
 })
